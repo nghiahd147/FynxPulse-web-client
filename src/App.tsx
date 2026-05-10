74cb5359c0fec7e2aa7030b2147370c5d3762389
nghiahd147 <qn500787@gmail.com>
2026-05-10 11:30:42 +0700

    style: update profile interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import "./App.css";
-import "./global.css";
 
 const App = () => <RouterProvider router={router} />;
 
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,11 +1,20 @@
-import { Camera } from "lucide-react";
+import { Button } from "antd";
+import {
+  BriefcaseBusiness,
+  Building2,
+  Camera,
+  Earth,
+  GraduationCap,
+  Pencil,
+  School,
+} from "lucide-react";
 
 const Profile = () => {
   return (
     <div className="w-full bg-bgPrimary">
       {/* Header Profile */}
       <div className="w-full flex flex-col items-center bg-white border-b border-bgPrimary z-10">
-        <div className="w-[60%]">
+        <div className="w-313">
           {/* Background */}
           <div className="w-full h-116.25 flex">
             <div className="flex-1 mx-auto relative">
@@ -21,18 +30,63 @@ const Profile = () => {
             </div>
           </div>
           {/* Avatar */}
-          <div className="w-full h-57.5 flex items-center">
+          <div className="w-full h-57.5 flex my-5">
             <div className="mx-7.5 relative">
               <img
                 src="https://i.pinimg.com/736x/58/ac/02/58ac02be97d79d173750d49d62b8f8fe.jpg"
                 alt="avatar-user"
                 className="w-50 h-50 rounded-[100%] cursor-pointer"
               />
-              <span className="bg-[#e2e5e9] hover:bg-bgPrimary transition-all ease-in absolute bottom-0 right-0 rounded-[100%] flex items-center justify-center cursor-pointer gap-x-2 p-3">
+              <span className="bg-[#e2e5e9] hover:bg-bgPrimary transition-all ease-in absolute bottom-9 right-3 rounded-[100%] flex items-center justify-center cursor-pointer gap-x-2 p-3">
                 <Camera />
               </span>
             </div>
-            <div></div>
+            <div className="pt-3 pr-1 pb-1 pl-3 flex flex-col">
+              {/* Avatar-top */}
+              <div className="flex justify-between">
+                <div className="flex flex-col my-2">
+                  <span className="font-bold text-4xl">Quang Nghĩa</span>
+                  <div className="flex items-center gap-x-2 font-bold">
+                    <span>120 người theo dõi</span>
+                    <span>•</span>
+                    <span>85 người đang theo dõi</span>
+                  </div>
+                </div>
+                <div className="flex items-center gap-x-2">
+                  <Button
+                    className="font-bold! bg-[#e2e5e9]! text-black!"
+                    type="primary"
+                    icon={<Pencil className="w-4 h-4" />}
+                  >
+                    Chỉnh sửa
+                  </Button>
+                </div>
+              </div>
+              <div className="mb-2 font-bold">...</div>
+              {/* Avatar Info */}
+              <div className="flex gap-x-3 font-bold text-sm">
+                <div className="flex items-center justify-center">
+                  <BriefcaseBusiness className="mr-1" />
+                  <span>Nhà thiết kế web</span>
+                </div>
+                <div className="flex items-center justify-center">
+                  <Earth className="mr-1" />
+                  <span>Hải Dương</span>
+                </div>
+                <div className="flex items-center justify-center">
+                  <Building2 className="mr-1" />
+                  <span>Công ty CP Công nghệ thông tin An Việt</span>
+                </div>
+                <div className="flex items-center justify-center">
+                  <School className="mr-1" />
+                  <span>THCS Ngọc Châu</span>
+                </div>
+                <div className="flex items-center justify-center">
+                  <GraduationCap className="mr-1" />
+                  <span>Trường Đại học Sao Đỏ</span>
+                </div>
+              </div>
+            </div>
           </div>
           <div></div>
         </div>
